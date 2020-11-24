9fe12794c2a0c37267360fd258ecf7e48ef8f962
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-11-24 23:13:43 +0300

    Внесение тестов

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/8738xg36iaud85it?svg=true)](https://ci.appveyor.com/project/AnastasiyaSergeichik/debitcardapplication)
--- src/test/java/DebitCardApplication.java
+++ src/test/java/DebitCardApplication.java
@@ -10,21 +10,22 @@ class DebitCardApplication {
         void shouldSubmitRequest() {
                 open("http://localhost:9999");
                 SelenideElement form = $(".form");
-                form.$("[data-test-id=name] input").setValue("Василий");
+                form.$("[data-test-id=name] input").setValue("Иван Петров");
                 form.$("[data-test-id=phone] input").setValue("+79270000000");
                 form.$("[data-test-id=agreement]").click();
                 form.$(".button").click();
-                $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+                $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
         }
-     //   @Test
-      //  void shouldValidated() {
-        //        open("http://localhost:9999");
-          //      SelenideElement form = $(".form");
-          //      form.$("[data-test-id=name] input").setValue("hjgfghfjghj377");
-           //     form.$("[data-test-id=agreement]").click();
-            //    form.$(".button").click();
-            //    $(".input_invalid .input__sub").shouldHave(exactText("Имя и фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    //    }
+        @Test
+        void shouldValidated() {
+                open("http://localhost:9999");
+                SelenideElement form = $(".form");
+                form.$("[data-test-id=name] input").setValue("Ivan Petrov");
+                form.$("[data-test-id=phone] input").setValue("+79270000000");
+                form.$("[data-test-id=agreement]").click();
+                form.$(".button").click();
+                $(".input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        }
 
 }
 
