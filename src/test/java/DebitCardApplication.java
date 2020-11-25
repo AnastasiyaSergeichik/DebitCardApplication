4a1c49b47e52906d3516be27c725f84868a5e556
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-11-25 10:47:12 +0300

    Внесение тестов

--- src/test/java/DebitCardApplication.java
+++ src/test/java/DebitCardApplication.java
@@ -6,28 +6,25 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class DebitCardApplication {
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иван Петров");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 
-        @Test
-        void shouldSubmitRequest() {
-                open("http://localhost:9999");
-                SelenideElement form = $(".form");
-                form.$("[data-test-id=name] input").setValue("Иван Петров");
-                form.$("[data-test-id=phone] input").setValue("+79270000000");
-                form.$("[data-test-id=agreement]").click();
-                form.$(".button").click();
-                $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-        }
-
-        @Test
-        void shouldValidated() {
-                open("http://localhost:9999");
-                SelenideElement form = $(".form");
-                form.$("[data-test-id=name] input").setValue("Ivan Petrov");
-                form.$("[data-test-id=phone] input").setValue("+79270000000");
-                form.$("[data-test-id=agreement]").click();
-                form.$(".button").click();
-                $(".input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        }
+    @Test
+    void shouldValidated() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ivan Petrov");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 }
-
-
