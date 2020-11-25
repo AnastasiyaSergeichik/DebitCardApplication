c3e1c7072bada86183bb92bef09b3faae5bc161b
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-11-25 21:09:47 +0300

    Корректировка

--- src/test/java/ru/netology/DebitCardApplication.java
+++ src/test/java/ru/netology/DebitCardApplication.java
@@ -13,7 +13,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class DebitCardApplication {
-
     private WebDriver driver;
 
     @BeforeAll
